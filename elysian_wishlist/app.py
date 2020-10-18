93794260d2a9be25ca75e692854cfb91d857c4e7
hhasan9979 <hhasan9979@gmail.com>
2020-10-18 17:24:29 -0400

    combined login and crud

--- elysian_wishlist/app.py
+++ elysian_wishlist/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, url_for, redirect, flash, \
 session, abort
 from flask_sqlalchemy import sqlalchemy, SQLAlchemy
 from modules.user_login.login_app import *
+from modules.crud.crud_Functions import *
 
 # Change dbname here
 #db_name = "auth.db"
@@ -23,7 +24,6 @@ def create_db():       #run this function if there is no current db created
 def signup():
     return api_signup(db)
 
-@app.route("/", methods=["GET", "POST"])
 @app.route("/login/", methods=["GET", "POST"])
 def login():
     return api_login(User)
@@ -36,7 +36,35 @@ def user_home(username):
 def logout(username):
     return api_logout(username)
 
+#route for all wishlists
+@app.route('/', methods=['POST', 'GET'])
+def allWishlists():
+    return index()
 
+#updates wishlist
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def updateWishlist(id):
+    return update(id)
+
+#delete wishlists and all sublists
+@app.route('/delete/<int:id>')
+def deleteWishlist(id):
+    return delete(id)
+
+#routes to their appropriate items for the wishlist
+@app.route('/list/<int:id>', methods=['POST', 'GET'])
+def wishlistItems(id):
+    return list(id)
+
+#deletes items from each wishlist
+@app.route('/deletesub/<int:id>')
+def deleteWishlistItems(id):
+    return deletesub(id)
+
+#update items from each wishlist
+@app.route('/updatesub/<int:id>', methods=['GET', 'POST'])
+def updateWishlistItems(id):
+    return updatesub(id)
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
