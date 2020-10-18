a69f257aa061a645c54feace78e6b86fd3ced4b9
Amrit Singh <asingh2066@gmail.com>
2020-10-17 22:43:30 -0400

    Add files via upload

--- flask-login/login_app.py
+++ flask-login/login_app.py
@@ -8,7 +8,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 db_name = "auth.db"
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{db}'.format(db=db_name)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auth.db'.format(db=db_name)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
@@ -18,9 +18,9 @@ app.config['SECRET_KEY'] = 'OCML3BRawWEUeaxcuKHLpw'
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
-    uid = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(100), nullable=False)
+class User(db.Model):                                   #creating a user model using flask sqlalchemy
+    uid = db.Column(db.Integer, primary_key=True)       #the id created when adding a session to the user
+    firstname = db.Column(db.String(100), nullable=False) #the information that will be stored in each corresponding column in the db
     lastname = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     username = db.Column(db.String(100), unique=True, nullable=False)
@@ -30,8 +30,7 @@ class User(db.Model):
         return '' % self.username
 
 
-def create_db():
-    """ # Execute this first time to create new db in current directory. """
+def create_db():       #run this function if there is no current db created
     db.create_all()
 
 
@@ -49,7 +48,7 @@ def signup():
             flash("Fields cannot be empty")
             return redirect(url_for('signup'))
         else:
-            firstname = firstname.strip()
+            firstname = firstname.strip()       #strip() removing all whitespaces
             lastname = lastname.strip()
             email = email.strip()
             username = username.strip()
@@ -58,11 +57,11 @@ def signup():
         # Returns salted pwd hash in format : method$salt$hashedvalue
         hashed_pwd = generate_password_hash(password, 'sha256')
 
-        new_user = User(firstname = firstname, lastname = lastname, email = email, username=username, pass_hash=hashed_pwd)
-        db.session.add(new_user)
-
+        new_user = User(firstname = firstname, lastname = lastname, email = email, username=username, pass_hash=hashed_pwd)  #creating new_user object
+        db.session.add(new_user)  #storing the object 'new_user' into the session, this allows for users to be authenticated to certain pages when requiring                         session[]=true
+                                  #for exampe, if session[amrit, singh, ...] = true, it will allow that user to login and access the homepage from there
         try:
-            db.session.commit()
+            db.session.commit()   #transaction is committed and user is given an id, new_user.id
         except sqlalchemy.exc.IntegrityError:
             flash("Email {e} is already in use.".format(e=email))
             flash("Username {u} is not available.".format(u=username))
@@ -79,7 +78,7 @@ def signup():
 def login():
 
     if request.method == "POST":
-        username = request.form['username']
+        username = request.form['username']  #session variable username and password are found and user can login
         password = request.form['password']
 
         if not (username and password):
@@ -91,8 +90,8 @@ def login():
 
         user = User.query.filter_by(username=username).first()
 
-        if user and check_password_hash(user.pass_hash, password):
-            session[username] = True
+        if user and check_password_hash(user.pass_hash, password):   #the check_password_hash function will check plaintext password with hashed value stored in database to see if they match
+            session[username] = True        #will store session cookie with true if username and password is valid
             return redirect(url_for("user_home", username=username))
         else:
             flash("Invalid username or password.")
@@ -102,11 +101,7 @@ def login():
 
 @app.route("/user/<username>/")
 def user_home(username):
-    """
-    Home page for validated users.
-
-    """
-    if not session.get(username):
+    if not session.get(username):   #if session cookie value is not true, it will not allow access to this page and return error 401
         abort(401)
   
     return render_template("user_home.html", username=username)
@@ -115,7 +110,7 @@ def user_home(username):
 @app.route("/logout/<username>")
 def logout(username):
     """ Logout user and redirect to login page with success message."""
-    session.pop(username, None)
+    session.pop(username, None)     #removes the current users session cookie when logging out, this pops out 'username' session variable
     flash("successfully logged out.")
     return redirect(url_for('login'))
 
