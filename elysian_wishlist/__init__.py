732b7735ab04704dafc9c496ac0d6a9dd21971a2
Naeem Meman <naeemmeman@gmail.com>
2020-11-17 23:50:14 -0500

    adding tests

--- elysian_wishlist/__init__.py
+++ elysian_wishlist/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auth.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# SECRET_KEY required for session, flash and Flask Sqlalchemy to work
-app.config['SECRET_KEY'] = 'OCML3BRawWEUeaxcuKHLpw'
-db = SQLAlchemy(app)
-db.create_all()
+db = SQLAlchemy()
 
+def create_app(db_uri='sqlite:///auth.db'):
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # SECRET_KEY required for session, flash and Flask Sqlalchemy to work
+    app.config['SECRET_KEY'] = 'OCML3BRawWEUeaxcuKHLpw'
+    db.init_app(app)
+    app.app_context().push()
+    db.create_all()
+  
+    return app
+
+app=create_app()
 from elysian_wishlist import routes
