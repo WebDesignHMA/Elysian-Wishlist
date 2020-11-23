14ab83bce00c28dbda9802e8b2d22c97c90f1141
Naeem <naeemmeman@gmail.com>
2020-11-23 17:53:23 -0500

    Update __init__.py

--- elysian_wishlist/__init__.py
+++ elysian_wishlist/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auth.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# SECRET_KEY required for session, flash and Flask Sqlalchemy to work
-app.config['SECRET_KEY'] = 'OCML3BRawWEUeaxcuKHLpw'
-db = SQLAlchemy(app)
-db.create_all()
+db = SQLAlchemy()
+def create_app(db_uri='postgres://gyrkqxkdikjkmt:8449569b8985bf996624c077e2f21e05d1cc814ae0601e93f4caa193febb471a@ec2-54-166-114-48.compute-1.amazonaws.com:5432/dfvhnt38h0rlsc'):
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # SECRET_KEY required for session, flash and Flask Sqlalchemy to work
+    app.config['SECRET_KEY'] = 'OCML3BRawWEUeaxcuKHLpw'
+    db.init_app(app)
+    app.app_context().push()
+    db.create_all()
+
+    return app
+
+app=create_app()
 
 from elysian_wishlist import routes
