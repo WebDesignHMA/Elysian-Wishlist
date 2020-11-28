a757efa4fc57c33f2c0d6cf7fe87ca485a02e6d8
Naeem <naeemmeman@gmail.com>
2020-11-27 19:04:22 -0500

    Update __init__.py

--- elysian_wishlist/__init__.py
+++ elysian_wishlist/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-def create_app(db_uri='postgres://gyrkqxkdikjkmt:8449569b8985bf996624c077e2f21e05d1cc814ae0601e93f4caa193febb471a@ec2-54-166-114-48.compute-1.amazonaws.com:5432/dfvhnt38h0rlsc'):
+def create_app(db_uri='postgres://uhfejtnaccuojq:960f111d3b5a84c8b548950c79e5ae6335bb21ab984dad9e5ceb563085fdc414@ec2-35-171-31-33.compute-1.amazonaws.com:5432/dj7uv0tb4n85s'):
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
