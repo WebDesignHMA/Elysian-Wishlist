f9801f9f47da9bde41ccdb0d9ca2289b3473696b
Naeem <naeemmeman@gmail.com>
2020-11-29 18:14:34 -0500

    Update test_elysian_wishlist.py

--- tests/test_elysian_wishlist.py
+++ tests/test_elysian_wishlist.py
@@ -1,7 +1,10 @@
-from elysian_wishlist.modules.third_party_api.amazon import *
-from elysian_wishlist.modules.third_party_api.ebay import *
+from elysian_wishlist.tests import app
+from elysian_wishlist.modules.third_party_api.ebay import ebay_search_catalog, ebay_search_item
 import json
-    
+
+def test_app():
+    assert app.config['SQLALCHEMY_DATABASE_URI']=='sqlite:///test.db'
+   
 def test_ebay_search_catalog():
     items=json.loads(ebay_search_catalog("baseball", 1))
     assert type(items)==type([])
@@ -13,4 +16,7 @@ def test_ebay_search_item():
     item=json.loads(ebay_search_item(id))
     assert type(item)==type({})
 
-    
+def test_signup():
+    with app.test_client() as client:
+        response = client.get('/home/')
+        print(response.data)
