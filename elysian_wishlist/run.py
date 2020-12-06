12e1f2d36c1080562f77929796fe27cbeb6a8c60
Hasibul Hasan <39868483+hhasan9979@users.noreply.github.com>
2020-12-06 11:42:28 -0500

    Update run.py

--- elysian_wishlist/run.py
+++ elysian_wishlist/run.py
@@ -4,7 +4,7 @@ from elysian_wishlist.modules.cron.cronFunctionality import *
 
 if __name__ == "__main__":
   
-    scheduler.add_job(id='Scheduled task', func=scheduledTask, trigger='interval', hours=12)
+    scheduler.add_job(id='Scheduled task', func=scheduledTask, trigger='interval', minutes=12)
     scheduler.start()
     #app.run(port=5000, debug=True)
     app.run(port=5000, debug=True, use_reloader=False)
