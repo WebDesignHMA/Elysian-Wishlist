ab6cf26a2998c5ec4bd79a35394d6faeb1cbf838
Naeem <naeemmeman@gmail.com>
2020-12-11 14:24:00 -0500

    Forgot to turn cron on.

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,6 @@ from elysian_wishlist.modules.cron.cronFunctionality import *
 
 
 if __name__ == "__main__":
-    #scheduler.add_job(id='Scheduled task', func=scheduledTask, trigger='interval', hours=5)
-    #scheduler.start()
+    scheduler.add_job(id='Scheduled task', func=scheduledTask, trigger='interval', hours=5)
+    scheduler.start()
     app.run(port=5000, debug=True, use_reloader=False)
